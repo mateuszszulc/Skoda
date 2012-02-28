4f6222d0bd83bbdc3efdbc50c27c72b7e31ff7bf
U-vermont\mateusz <mateusz@vermont.(none)>
2012-02-29 00:05:33 +0100

    ArrayTests - small modification.

--- src/testing/arrays/ArrayTests.java
+++ src/testing/arrays/ArrayTests.java
@@ -1,10 +1,31 @@
 package testing.arrays;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by IntelliJ IDEA.
  * User: mateusz
  * Date: 28.02.12
  * Time: 23:47
  */
+
 public class ArrayTests {
+    public static void main(String[] args) {
+        int[] intList = new int[10];
+        intList[9] = 5;
+//        for (int i : intList )
+//        {
+//            System.out.println("");
+//        }
+
+        List<Integer> lista = new ArrayList();
+        lista.add(1);
+        lista.add(2);
+
+        for (Integer i : lista) {
+            System.out.println(i);
+        }
+
+    }
 }
